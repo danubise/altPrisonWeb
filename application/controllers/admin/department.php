1a2af56705fb7e7279dbabface0d5772933bb9b2
danubise <danubise@gmail.com>
2018-06-11 12:08:44 +0400

    Добавление управлений

--- application/controllers/admin/department.php
+++ application/controllers/admin/department.php
@@ -1,3 +1,5 @@
+<?
+
 class Department extends Core_controller {
     public function __construct() {
         parent::__construct();
@@ -6,12 +8,46 @@ class Department extends Core_controller {
 
     public function index() {
     $departments = $this->db->select("* from `groups`");
+    var_dump($departments);
        $this->view(
             array(
                 'view' => 'department/index',
-                'departments' => $departments
+                'var' => array(
+                    'departments' => $departments
+                )
             )
        );
+    }
+
+    public function add() {
+       $this->view(
+            array(
+                'view' => 'department/add'
+            )
+       );
+    }
+
+    public function adding() {
+        $this->db->insert("groups",$_POST['department']);
+        $this->index();
+    }
+
+    public function edit($groupid) {
+        $department = $this->db->select("* from `groups` where groupid='".$groupid."'");
+        $numbers = $this->db->select("* from `phonenumbers` where groupid='".$groupid."'");
+        printarray($department);
+        printarray($numbers);
+        $this->view(
+            array(
+                'view' => 'department/edit'
+            )
+        );
+    }
+
+
+    public function delete($groupid) {
+        $this->db->delete("from groups where groupid='".$groupid."'");
+        $this->index();
     }
 
     public function logout() {
@@ -19,3 +55,4 @@ class Department extends Core_controller {
         header('Location: '.baseurl());
     }
 }
+?>
--- application/views/admin/department/index.php
+++ application/views/admin/department/index.php
@@ -1,3 +1,20 @@
 <?
-printarray( $departments);
+printarray($departments);
 ?>
+<a href="<?=baseurl('department/add/')?>" class="btn btn-success">Создать учреждение</a>
+
+<table class="table table-striped">
+    <tr>
+        <th>ID</th><th>Название</th><th>Emails</th><th>Операции</th>
+    </tr>
+<?php
+    foreach($departments as $key=>$data){
+        echo "<tr><td>".$data['groupid']."</td><td>".$data['name']."</td><td>".$data['emails']."</td><td>";
+
+        echo "<a href=\"".baseurl('department/edit/'.$data['groupid'])."\">Изменить</a> / ";
+        echo "<a href=\"".baseurl('department/delete/'.$data['groupid'])."\">Удалить</a></td></tr>";
+
+
+    }
+?>
+</table>
