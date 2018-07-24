540931ff9c107de211cc6d65087d7ae349bcb998
danubise <danubise@gmail.com>
2018-07-24 11:13:55 +0400

    Вернул групповое добавление номеров через запятую

--- application/controllers/admin/department.php
+++ application/controllers/admin/department.php
@@ -105,21 +105,21 @@ class Department extends Core_controller {
             if (strpos($_POST['number']['phone'],",") === false && trim($_POST['number']['phone']) != "" && ctype_digit($_POST['number']['phone'])){
                 $groupid = $this->db->insert("phonenumbers", $_POST['number']);
             }else{
-//                $edelveys="";
-//                if(isset($_POST['number']['edelveys'])){
-//                    $edelveys=$_POST['number']['edelveys'];
-//                }
-//                $numbers=explode(",",$_POST['number']['phone']);
-//                foreach($numbers as $key=>$number){
-//                    $number=trim($number);
-//                    if( $number != "" && ctype_digit($number)){
-//                        $this->db->insert("phonenumbers", array(
-//                            'edelveys'=>$edelveys,
-//                            'phone'=> $number,
-//                            'groupid'=>$_POST['number']['groupid']
-//                            ));
-//                        }
-//                }
+                $edelveys="";
+                if(isset($_POST['number']['edelveys'])){
+                    $edelveys=$_POST['number']['edelveys'];
+                }
+                $numbers=explode(",",$_POST['number']['phone']);
+                foreach($numbers as $key=>$number){
+                    $number=trim($number);
+                    if( $number != "" && ctype_digit($number)){
+                        $this->db->insert("phonenumbers", array(
+                            'edelveys'=>$edelveys,
+                            'phone'=> $number,
+                            'groupid'=>$_POST['number']['groupid']
+                            ));
+                        }
+                }
             }
         }
         $this->edit($_POST['number']['groupid']);
